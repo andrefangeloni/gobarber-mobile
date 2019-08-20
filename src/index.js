bf395cc83827afa42465e67bb74028dd2f9bdcad
andrefangeloni <andrefangeloni@gmail.com>
2019-08-19 21:43:07 -0300

    Rotas de autenticação criadas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import { View } from 'react-native';
-
-// import { Container } from './styles';
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View />
-  );
+  return <Routes />;
 }
