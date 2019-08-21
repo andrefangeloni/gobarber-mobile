9afd618b58c0eedb2b0c878d2fd6b2d74eebf106
andrefangeloni <andrefangeloni@gmail.com>
2019-08-21 20:51:25 -0300

    Melhorias na UX de SignIn e SignUp

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Text, Image} from 'react-native';
+import React, {useRef} from 'react';
+import {Image} from 'react-native';
 import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
 import {
@@ -12,6 +12,10 @@ import {
 } from './styles';
 
 export default function SignIn({navigation}) {
+  const passwordRef = useRef();
+
+  function handleSubmit() {}
+
   return (
     <Background>
       <Container>
@@ -24,14 +28,19 @@ export default function SignIn({navigation}) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Digite seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
           />
           <FormInput
             icon="lock-outline"
             secureTextEntry
             placeholder="Digite sua senha"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
           />
 
-          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
         </Form>
 
         <SignLink onPress={() => navigation.navigate('SignUp')}>
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Text, Image} from 'react-native';
+import React, {useRef} from 'react';
+import {Image} from 'react-native';
 import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
 import {
@@ -12,6 +12,11 @@ import {
 } from './styles';
 
 export default function SignUp({navigation}) {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  function handleSubmit() {}
+
   return (
     <Background>
       <Container>
@@ -22,6 +27,8 @@ export default function SignUp({navigation}) {
             icon="person-outline"
             autoCorrect={false}
             placeholder="Digite seu nome"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
           />
           <FormInput
             icon="mail-outline"
@@ -29,11 +36,17 @@ export default function SignUp({navigation}) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Digite seu e-mail"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
           />
           <FormInput
             icon="lock-outline"
             secureTextEntry
             placeholder="Digite sua senha"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
           />
 
           <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
