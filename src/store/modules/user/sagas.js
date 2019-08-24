8dc021a3d05df42d634814edaea227d2ba150bd1
andrefangeloni <andrefangeloni@gmail.com>
2019-08-23 22:49:01 -0300

    Tela de profile estilizada e funcionando com o Redux

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,13 +1,13 @@
-import {Alert} from 'react-native';
-import {takeLatest, call, put, all} from 'redux-saga/effects';
+import { Alert } from 'react-native';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 import api from '~/services/api';
-import {updateProfileSuccess, updateProfileFailure} from './actions';
+import { updateProfileSuccess, updateProfileFailure } from './actions';
 
 export function* updateProfile({payload}) {
   try {
-    const {name, email, avatar_id, ...rest} = payload.data;
+    const { name, email, ...rest } = payload.data;
     const profile = Object.assign(
-      {name, email, avatar_id},
+      { name, email },
       rest.oldPassword ? rest : {},
     );
     const response = yield call(api.put, '/users', profile);
